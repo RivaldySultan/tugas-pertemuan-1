09145f2e70c51c14a655aead7711b810be518b31
RivaldySultan <115138860+RivaldySultan@users.noreply.github.com>
2023-09-27 12:28:58 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,13 @@
+const data = {
+    nama : "Rivaldy",
+    nim : 20220040270
+};
+const SKS = {
+    sem1 : 20,
+    sem2 : 21
+};
+
+console.log("nama saya adalah : ", data.nama)
+console.log("nim saya adalah : ",data.nim)
+console.log("SKS yang saya tempuh 2 semester adalah : ",SKS.sem1 + SKS.sem2)
+
